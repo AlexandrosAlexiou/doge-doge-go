6770488ac4abe9d1076e2d2fe57c5eebcf654665
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2021-05-14 18:23:18 +0300

    Create API endpoint that returns suggestions based on a term

--- backend/src/main/java/uoi/cs/searchengine/lucene/DogeDogeGoSpellChecker.java
+++ backend/src/main/java/uoi/cs/searchengine/lucene/DogeDogeGoSpellChecker.java
@@ -11,26 +11,22 @@ import uoi.cs.searchengine.ApplicationConstants;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DogeDogeGoSpellChecker {
 
 
-    public void check_txt_dictionary(String input_word) throws IOException
-    {
+    public List<String> suggest(String input_word) throws IOException {
         // Creating the index
         Directory directory = FSDirectory.open(new File(ApplicationConstants.INDEX_PATH).toPath());
         //PlainTextDictionary txt_dict = new PlainTextDictionary(Paths.get(ApplicationConstants.ENGLISH_DICTIONARY));
         SpellChecker checker = new SpellChecker(directory);
-
         IndexReader reader = DirectoryReader.open(directory);
         //checker.indexDictionary(txt_dict, new IndexWriterConfig(new DogeDogeGoAnalyzer()), false);
         checker.indexDictionary(new LuceneDictionary(reader, "text"), new IndexWriterConfig(new DogeDogeGoAnalyzer()), true);
-
         directory.close();
-
-
 
         // Searching and presenting the suggested words by selecting a string distance
         //checker.setStringDistance(new JaroWinklerDistance());
@@ -40,20 +36,16 @@ public class DogeDogeGoSpellChecker {
 
         String[] suggestions = checker.suggestSimilar(input_word, 10);
 
-        System.out.println("By '" + input_word + "' did you mean:");
-        for(String suggestion : suggestions)
-            System.out.println("\t" + suggestion);
+        // System.out.println("By '" + input_word + "' did you mean:");
+        return Arrays.asList(suggestions);
     }
 
 
-    public static void main(String[] args) throws IOException, Throwable
-    {
+    public static void main(String[] args) throws IOException, Throwable {
         Scanner scan = new Scanner(System.in);
-        DogeDogeGoSpellChecker spell_checker = new DogeDogeGoSpellChecker();
-
+        DogeDogeGoSpellChecker spellChecker = new DogeDogeGoSpellChecker();
         System.out.print("\nType a word to spell check: ");
         String input_word = scan.next();
-
-        spell_checker.check_txt_dictionary(input_word);
+        System.out.println(spellChecker.suggest(input_word));
     }
 }
--- backend/src/main/java/uoi/cs/searchengine/web/SearchController.java
+++ backend/src/main/java/uoi/cs/searchengine/web/SearchController.java
@@ -3,11 +3,13 @@ package uoi.cs.searchengine.web;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import uoi.cs.searchengine.lucene.DogeDogeGoSpellChecker;
 import uoi.cs.searchengine.lucene.Searcher;
 import uoi.cs.searchengine.model.Article;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @Controller
@@ -17,10 +19,22 @@ public class SearchController {
     @RequestMapping("/query")
     @ResponseBody
     public ArrayList<Article> search(@RequestParam String q) throws Exception {
-        try{
+        try {
             Searcher iSearcher = new Searcher();
-             return iSearcher.searchAndHighlight(q);
+            return iSearcher.searchAndHighlight(q);
         } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    @RequestMapping("/suggestions")
+    @ResponseBody
+    public List<String> suggest(@RequestParam String t) {
+        try {
+            DogeDogeGoSpellChecker spellChecker = new DogeDogeGoSpellChecker();
+            return spellChecker.suggest(t);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return new ArrayList<>();
